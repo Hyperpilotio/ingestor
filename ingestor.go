d95b006528e4ba1dd66b3d1d4097c7a3c58319c2
alan.yang <jpra1113@gmail.com>
2017-01-23 18:03:07 +0800

    add ingestor main

---
+++ ingestor.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/golang/glog"
+
+	"github.com/spf13/viper"
+)
+
+// Run start the web server
+func Run(fileConfig string) error {
+	viper := viper.New()
+	viper.SetConfigType("json")
+
+	if fileConfig == "" {
+		viper.SetConfigName("config")
+		viper.AddConfigPath("/etc/ingestor")
+	} else {
+		viper.SetConfigFile(fileConfig)
+	}
+
+	viper.SetDefault("port", "7780")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	server := NewServer(viper)
+	return server.StartServer()
+}
+
+func main() {
+	configPath := flag.String("config", "", "The file path to a config file")
+	flag.Parse()
+
+	err := Run(*configPath)
+	glog.Errorln(err)
+}
