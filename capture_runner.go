d394c8c6f203d2b6961392481d6518e59017c567
alan.yang <jpra1113@gmail.com>
2017-01-25 17:21:21 +0800

    update AWS ECS capture data struct

--- capture_runner.go
+++ capture_runner.go
@@ -8,10 +8,13 @@ import (
 func RunCapture(config *viper.Viper) {
 	db, _ := ConnectDB(config)
 	for _, regionName := range capturer.Regions {
+		// capture AWS ECS Clusters
 		deployments, _ := capturer.GetClusters(config, regionName)
 		if deployments != nil {
 			db.Insert(*deployments)
 		}
+
+		// TODO: capture other....
 	}
 	db.Close()
 }
--- capturer/awsecs.go
+++ capturer/awsecs.go
@@ -40,12 +40,34 @@ type NodeInfo struct {
 	PublicDnsName string   `json:"PublicDnsName" bson:"PublicDnsName"`
 }
 
+type Service struct {
+	ServiceName    string `json:"ServiceName" bson:"ServiceName"`
+	ServiceArn     string `json:"ServiceArn" bson:"ServiceArn"`
+	TaskDefinition string `json:"TaskDefinition" bson:"TaskDefinition"`
+}
+
+type Container struct {
+	ContainerArn string `json:"ContainerArn" bson:"ContainerArn"`
+	Name         string `json:"Name" bson:"Name"`
+}
+
+type Task struct {
+	Containers        []Container `json:"Containers" bson:"Containers"`
+	TaskArn           string      `json:"TaskArn" bson:"TaskArn"`
+	TaskDefinitionArn string      `json:"TaskDefinitionArn" bson:"TaskDefinitionArn"`
+}
+
+type Cluster struct {
+	ClusterName string           `json:"ClusterName" bson:"ClusterName"`
+	NodeInfos   map[int]NodeInfo `json:"NodeInfos" bson:"NodeInfos"`
+	Services    []Service        `json:"Services" bson:"Services"`
+	Tasks       []Task           `json:"Tasks" bson:"Tasks"`
+}
+
 type Deployments struct {
-	ID             bson.ObjectId    `json:"id" bson:"_id"`
-	Region         string           `json:"Region" bson:"Region"`
-	InstanceNumber int              `json:"InstanceNumber" bson:"InstanceNumber"`
-	NodeInfos      map[int]NodeInfo `json:"NodeInfos" bson:"NodeInfos"`
-	Tasks          []string         `json:"Tasks" bson:"Tasks"`
+	ID       bson.ObjectId `json:"id" bson:"_id"`
+	Region   string        `json:"Region" bson:"Region"`
+	Clusters []Cluster     `json:"Clusters" bson:"Clusters"`
 }
 
 func createSessionByRegion(viper *viper.Viper, regionName string) (*session.Session, error) {
@@ -66,48 +88,130 @@ func createSessionByRegion(viper *viper.Viper, regionName string) (*session.Sess
 }
 
 func GetClusters(viper *viper.Viper, regionName string) (*Deployments, error) {
-	glog.V(1).Infof("GetClusters")
+	glog.V(1).Infof("GetClusters for region: %s", regionName)
 	deployments := &Deployments{ID: bson.NewObjectId(), Region: regionName}
 	sess, sessionErr := createSessionByRegion(viper, regionName)
 	if sessionErr != nil {
 		return nil, errors.New("Unable to create session: " + sessionErr.Error())
 	}
 
-	ec2Svc := ec2.New(sess)
 	ecsSvc := ecs.New(sess)
+	ec2Svc := ec2.New(sess)
 
-	describeInstanceOutput, _ := ec2Svc.DescribeInstances(nil)
-	if describeInstanceOutput.Reservations == nil {
-		return nil, errors.New("Can not find instance in the " + regionName)
+	// find clusters on region
+	listClustersOutput, listClustersErr := ecsSvc.ListClusters(nil)
+	if listClustersErr != nil {
+		return nil, errors.New("Unable to find any clusters: " + listClustersErr.Error())
 	}
 
-	deployments.InstanceNumber = len(describeInstanceOutput.Reservations)
-	nodeInfos := map[int]NodeInfo{}
+	// use clusterArns get region's clusters information
+	describeClustersInput := &ecs.DescribeClustersInput{
+		Clusters: listClustersOutput.ClusterArns,
+	}
+	describeClustersOutput, _ := ecsSvc.DescribeClusters(describeClustersInput)
 
-	for idx, reservation := range describeInstanceOutput.Reservations {
-		nodeInfo := &NodeInfo{}
-		deployInstance := &Instance{}
+	deployClusters := []Cluster{}
+	for _, cluster := range describeClustersOutput.Clusters {
+		clusterName := *cluster.ClusterName
+		deployCluster := &Cluster{}
+		deployCluster.ClusterName = clusterName
 
-		for _, instance := range reservation.Instances {
-			nodeInfo.PublicDnsName = *instance.PublicDnsName
-			nodeInfo.Arn = *instance.IamInstanceProfile.Arn
+		// use clusterName get instancess arns of Container
+		listInstancesInput := &ecs.ListContainerInstancesInput{
+			Cluster: aws.String(clusterName),
+		}
+		listContainerInstancesOutput, _ := ecsSvc.ListContainerInstances(listInstancesInput)
 
+		// use containerInstanceArns get ContainerInstances
+		describeInstancesInput := &ecs.DescribeContainerInstancesInput{
+			Cluster:            aws.String(clusterName),
+			ContainerInstances: listContainerInstancesOutput.ContainerInstanceArns,
+		}
+		describeInstancesOutput, _ := ecsSvc.DescribeContainerInstances(describeInstancesInput)
+
+		// use Ec2InstanceId get instance information
+		nodeInfos := map[int]NodeInfo{}
+		for idx, containerInstance := range describeInstancesOutput.ContainerInstances {
+			ec2InstanceId := *containerInstance.Ec2InstanceId
+			describeInstancesInput := &ec2.DescribeInstancesInput{
+				InstanceIds: []*string{
+					aws.String(ec2InstanceId),
+				},
+			}
+			describeInstanceOutput, _ := ec2Svc.DescribeInstances(describeInstancesInput)
+
+			reservation := describeInstanceOutput.Reservations[0]
+			instance := reservation.Instances[0]
+
+			deployInstance := &Instance{}
 			deployInstance.InstanceId = *instance.InstanceId
 			deployInstance.InstanceType = *instance.InstanceType
 			deployInstance.LaunchTime = *instance.LaunchTime
+
+			nodeInfo := &NodeInfo{}
+			nodeInfo.PublicDnsName = *instance.PublicDnsName
+			nodeInfo.Arn = *instance.IamInstanceProfile.Arn
+			nodeInfo.Instance = *deployInstance
+			nodeInfos[idx+1] = *nodeInfo
 		}
+		deployCluster.NodeInfos = nodeInfos
 
-		nodeInfo.Instance = *deployInstance
-		nodeInfos[idx+1] = *nodeInfo
-	}
-	deployments.NodeInfos = nodeInfos
+		// use clusterName get ServiceArns
+		listServicesInput := &ecs.ListServicesInput{
+			Cluster: aws.String(clusterName),
+		}
+		listServicesOutput, _ := ecsSvc.ListServices(listServicesInput)
 
-	tasks := []string{}
-	listTaskDefinitionFamiliesOutput, _ := ecsSvc.ListTaskDefinitionFamilies(nil)
-	for _, familie := range listTaskDefinitionFamiliesOutput.Families {
-		tasks = append(tasks, *familie)
+		// use ServiceArns get service information
+		describeServicesInput := &ecs.DescribeServicesInput{
+			Services: listServicesOutput.ServiceArns,
+			Cluster:  aws.String(clusterName),
+		}
+		describeServicesOutput, _ := ecsSvc.DescribeServices(describeServicesInput)
+
+		clusterServices := []Service{}
+		for _, service := range describeServicesOutput.Services {
+			clusterService := &Service{}
+			clusterService.ServiceArn = *service.ServiceArn
+			clusterService.ServiceName = *service.ServiceName
+			clusterService.TaskDefinition = *service.TaskDefinition
+			clusterServices = append(clusterServices, *clusterService)
+		}
+		deployCluster.Services = clusterServices
+
+		// use clusterName get TaskArns
+		listTasksInput := &ecs.ListTasksInput{
+			Cluster: aws.String(clusterName),
+		}
+		listTasksOutput, _ := ecsSvc.ListTasks(listTasksInput)
+
+		// use TaskArns get task information
+		describeTasksInput := &ecs.DescribeTasksInput{
+			Tasks:   listTasksOutput.TaskArns,
+			Cluster: aws.String(clusterName),
+		}
+		describeTasksOutput, _ := ecsSvc.DescribeTasks(describeTasksInput)
+
+		clusterTasks := []Task{}
+		for _, task := range describeTasksOutput.Tasks {
+			clusterTask := &Task{}
+			clusterTask.TaskArn = *task.TaskArn
+			clusterTask.TaskDefinitionArn = *task.TaskDefinitionArn
+
+			taskContainers := []Container{}
+			for _, container := range task.Containers {
+				taskContainer := &Container{}
+				taskContainer.ContainerArn = *container.ContainerArn
+				taskContainer.Name = *container.Name
+				taskContainers = append(taskContainers, *taskContainer)
+			}
+			clusterTask.Containers = taskContainers
+			clusterTasks = append(clusterTasks, *clusterTask)
+		}
+		deployCluster.Tasks = clusterTasks
+		deployClusters = append(deployClusters, *deployCluster)
 	}
-	deployments.Tasks = tasks
+	deployments.Clusters = deployClusters
 
 	return deployments, nil
 }
